ec44518087ba5b62c9418d30a7510503cc37601c
Alexey Shevchenko <kamikozz@mail.ru>
2020-02-16 02:10:43 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,21 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const LN_2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  // check if string, not negative & not zero
+  if (typeof sampleActivity !== 'string' ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY)
+    return false;
+
+  // check for characters (except '.') in the string
+  for (let i = 0; i < sampleActivity.length; i++)
+    if (!(sampleActivity[i] >= 0 && sampleActivity[i] < 10) &&
+      sampleActivity[i] !== '.')
+      return false;
+
+  const RATE_HALF_LIFE = LN_2 / HALF_LIFE_PERIOD;
+  let date = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / RATE_HALF_LIFE;
+  return Math.ceil(date);
 };
